3d3f59315839cfb9091941f16e973b727218063a
Alberto Mercurio <alberto.mercurio96@gmail.com>
2024-05-30 23:12:37 +0200

    Minor changes

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,18 @@
 using NeuralQuantumStates
 using Test
-using Aqua
-using JET
 
-@testset "NeuralQuantumStates.jl" begin
-    @testset "Code quality (Aqua.jl)" begin
-        Aqua.test_all(NeuralQuantumStates; ambiguities = false,)
+
+if ((GROUP == "All") || (GROUP == "Code-Quality")) && (VERSION >= v"1.9")
+    Pkg.add(["Aqua", "JET"])
+    using Aqua
+    using JET
+    @testset "NeuralQuantumStates.jl" begin
+        @testset "Code quality (Aqua.jl)" begin
+            Aqua.test_all(NeuralQuantumStates; ambiguities = false)
+        end
+        @testset "Code linting (JET.jl)" begin
+            JET.test_package(NeuralQuantumStates; target_defined_modules = true)
+        end
+        # Write your tests here.
     end
-    @testset "Code linting (JET.jl)" begin
-        JET.test_package(NeuralQuantumStates; target_defined_modules = true)
-    end
-    # Write your tests here.
 end
