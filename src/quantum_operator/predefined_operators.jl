aa581e9e1df8af6623bb54070cd0f28ad587dee0
Alberto Mercurio <alberto.mercurio96@gmail.com>
2024-05-31 21:25:45 +0200

    Format project

--- src/quantum_operator/predefined_operators.jl
+++ src/quantum_operator/predefined_operators.jl
@@ -4,6 +4,9 @@ _sigma_x_mat(T::Type{<:Number} = default_eltype) = spdiagm(1 => T[1], -1 => T[1]
 _sigma_y_mat(T::Type{<:Number} = default_eltype) = spdiagm(1 => T[1im], -1 => T[-1im])
 _sigma_z_mat(T::Type{<:Number} = default_eltype) = spdiagm(0 => T[-1, 1])
 
-sigmax(hilbert::Hilbert, acting_on::Int, T::Type{<:Number} = default_eltype) = QuantumOperator(hilbert, acting_on, _sigma_x_mat(T))
-sigmay(hilbert::Hilbert, acting_on::Int, T::Type{<:Number} = default_eltype) = QuantumOperator(hilbert, acting_on, _sigma_y_mat(T))
-sigmaz(hilbert::Hilbert, acting_on::Int, T::Type{<:Number} = default_eltype) = QuantumOperator(hilbert, acting_on, _sigma_z_mat(T))
+sigmax(hilbert::Hilbert, acting_on::Int, T::Type{<:Number} = default_eltype) =
+    QuantumOperator(hilbert, acting_on, _sigma_x_mat(T))
+sigmay(hilbert::Hilbert, acting_on::Int, T::Type{<:Number} = default_eltype) =
+    QuantumOperator(hilbert, acting_on, _sigma_y_mat(T))
+sigmaz(hilbert::Hilbert, acting_on::Int, T::Type{<:Number} = default_eltype) =
+    QuantumOperator(hilbert, acting_on, _sigma_z_mat(T))
